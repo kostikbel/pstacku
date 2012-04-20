27055d97fdf2384038e320c4ab3acb06e67aff46
Kostik Belousov <kostik@sirion>
2012-04-20 16:37:30 +0300

    Auto-detect my libunwind install dir.

--- configure.sh
+++ configure.sh
@@ -2,6 +2,13 @@
 
 CC=/usr/local/opt/gcc-4.7.0/bin/gcc; export CC
 CFLAGS="-Wall -Wextra -O -g"; export CFLAGS
+
+case $(sysctl -n hw.machine) in
+    amd64) bitness=64 ;;
+    i386)  bitness=32 ;;
+    *) echo "Unknown arch"; exit 1;;
+esac
+
 ../pstacku/configure \
     --enable-maintainer-mode \
-    --with-libunwind=/usr/home/kostik/build/bsd/libunwind/usr32
+    --with-libunwind=/usr/home/kostik/build/bsd/libunwind/usr$bitness
