0807779180afda4168a554bc30a0edb9aa275d64
Konstantin Belousov <kib@freebsd.org>
2012-04-21 20:17:13 +0300

    Fix args retrieval calculation on amd64.

--- pstack-amd64.c
+++ pstack-amd64.c
@@ -1,4 +1,5 @@
 #include <err.h>
+
 #include <libunwind.h>
 
 #include "pstack.h"
@@ -8,10 +9,10 @@ pstack_get_arg0(unw_cursor_t *c, unw_word_t *arg)
 {
 	int ret;
 
-	ret = unw_get_reg(c, UNW_REG_RDI, &ip);
+	ret = unw_get_reg(c, UNW_X86_64_RDI, arg);
 	if (ret < 0) {
 		if (verbose) {
-			warnx("unw_get_reg(UNW_REG_RDI) failed, %s",
+			warnx("unw_get_reg(UNW_X86_64_RDI) failed, %s",
 			      unw_strerror(ret));
 		}
 		return (0);
@@ -24,10 +25,10 @@ pstack_get_arg1(unw_cursor_t *c, unw_word_t *arg)
 {
 	int ret;
 
-	ret = unw_get_reg(c, UNW_REG_RSI, &ip);
+	ret = unw_get_reg(c, UNW_X86_64_RSI, arg);
 	if (ret < 0) {
 		if (verbose) {
-			warnx("unw_get_reg(UNW_REG_RSI) failed, %s",
+			warnx("unw_get_reg(UNW_X86_64_RSI) failed, %s",
 			      unw_strerror(ret));
 		}
 		return (0);
@@ -40,10 +41,10 @@ pstack_get_arg2(unw_cursor_t *c, unw_word_t *arg)
 {
 	int ret;
 
-	ret = unw_get_reg(c, UNW_REG_RDX, &ip);
+	ret = unw_get_reg(c, UNW_X86_64_RDX, arg);
 	if (ret < 0) {
 		if (verbose) {
-			warnx("unw_get_reg(UNW_REG_RDX) failed, %s",
+			warnx("unw_get_reg(UNW_X86_64_RDX) failed, %s",
 			      unw_strerror(ret));
 		}
 		return (0);
@@ -56,10 +57,10 @@ pstack_get_arg3(unw_cursor_t *c, unw_word_t *arg)
 {
 	int ret;
 
-	ret = unw_get_reg(c, UNW_REG_RCX, &ip);
+	ret = unw_get_reg(c, UNW_X86_64_RCX, arg);
 	if (ret < 0) {
 		if (verbose) {
-			warnx("unw_get_reg(UNW_REG_RCX) failed, %s",
+			warnx("unw_get_reg(UNW_X86_64_RCX) failed, %s",
 			      unw_strerror(ret));
 		}
 		return (0);
@@ -72,10 +73,10 @@ pstack_get_arg4(unw_cursor_t *c, unw_word_t *arg)
 {
 	int ret;
 
-	ret = unw_get_reg(c, UNW_REG_R8, &ip);
+	ret = unw_get_reg(c, UNW_X86_64_R8, arg);
 	if (ret < 0) {
 		if (verbose) {
-			warnx("unw_get_reg(UNW_REG_R8) failed, %s",
+			warnx("unw_get_reg(UNW_X86_64_R8) failed, %s",
 			      unw_strerror(ret));
 		}
 		return (0);
@@ -88,10 +89,10 @@ pstack_get_arg5(unw_cursor_t *c, unw_word_t *arg)
 {
 	int ret;
 
-	ret = unw_get_reg(c, UNW_REG_R9, &ip);
+	ret = unw_get_reg(c, UNW_X86_64_R9, arg);
 	if (ret < 0) {
 		if (verbose) {
-			warnx("unw_get_reg(UNW_REG_R9) failed, %s",
+			warnx("unw_get_reg(UNW_X86_64_R9) failed, %s",
 			      unw_strerror(ret));
 		}
 		return (0);
