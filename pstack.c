aec2762d773a9d325a6892f23ca8d34bc46a6b18
Konstantin Belousov <kib@freebsd.org>
2012-04-21 15:18:04 +0300

    Fix frame number calculation.

--- pstack.c
+++ pstack.c
@@ -68,6 +68,7 @@ backtrace_lwp(unw_addr_space_t as, void *ui, lwpid_t lwpid)
 			    "unw_step() error for ip %0lx/start ip %0lx, %s",
 			    (long)ip, (long)start_ip, unw_strerror(ret));
 		}
+		n++;
 	} while (ret > 0);
 }
 
