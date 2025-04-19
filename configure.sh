c78d971cab835cb51ff30641ae7096a7f26a6883
Konstantin Belousov <kib@FreeBSD.org>
2025-04-19 16:51:28 +0300

    Update the local configure call from the reality

--- configure.sh
+++ configure.sh
@@ -1,14 +1,8 @@
 #!/usr/local/bin/bash
 
-CC=/usr/local/opt/gcc-4.7.2/bin/gcc; export CC
+CC=/usr/local/opt/gcc-14.2.0/bin/gcc; export CC
 CFLAGS="-Wall -Wextra -O -g"; export CFLAGS
-
-case $(sysctl -n hw.machine) in
-    amd64) bitness=64 ;;
-    i386)  bitness=32 ;;
-    *) echo "Unknown arch"; exit 1;;
-esac
 
 ../pstacku/configure \
     --enable-maintainer-mode \
-    --with-libunwind=/usr/home/kostik/build/bsd/libunwind/usr$bitness
+    --with-libunwind=/usr/local
